eaad73bfe35901e5423a1e6af22f81406eaa402a
Yeeanj <sesju0527@gamil.com>
2023-08-30 09:56:24 +0900

    08.29

--- dist/js/comm.js
+++ dist/js/comm.js
@@ -1,35 +1,45 @@
-var swiper = new Swiper(".slide_intro", {
-  spaceBetween: 40,
-  centeredSlides: true,
-  slidesPerView: 1,
-  loop: true,
-  loopAdditionalSlides: 1,
+var splide = new Splide(".main_slide", {
+  type: "loop",
+  // perPage: 1,
+  // arrows: false,
+  gap: "40px",
+  start: 0,
   autoplay: {
-    delay: 3500,
-    disableOnInteraction: false,
-  },
-  pagination: {
-    el: ".swiper-pagination",
-    dynamicBullets: true,
+    interval: 300,
+    pauseOnHover: {
+      resetProgress: true,
+    },
   },
 });
+
+splide.mount();
+
+// var swiper = new Swiper(".slide_intro", {
+//   spaceBetween: 40,
+//   centeredSlides: true,
+//   slidesPerView: 1,
+//   loop: true,
+//   loopAdditionalSlides: 1,
+//   autoplay: {
+//     delay: 3500,
+//     disableOnInteraction: false,
+//   },
+//   pagination: {
+//     el: ".swiper-pagination",
+//     dynamicBullets: true,
+//   },
+// });
 
 var swiper = new Swiper(".event_reser", {
   slidesPerView: 5,
   spaceBetween: 20,
   loop: true,
   autoplay: {
-    delay: 3000,
+    delay: 4000,
     disableOnInteraction: false,
   },
   scrollbar: {
     el: ".swiper-scrollbar",
     hide: false,
   },
-});
-
-var swiper = new Swiper(".artist_lst", {
-  slidesPerView: 3,
-  spaceBetween: 20,
-  loop: true,
 });
