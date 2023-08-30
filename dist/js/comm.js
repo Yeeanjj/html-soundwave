0c8d3bb88e34f5a48d652255a31f71cb41e1035e
Yeeanj <sesju0527@gamil.com>
2023-08-30 12:58:33 +0900

    z

--- dist/js/comm.js
+++ dist/js/comm.js
@@ -4,12 +4,12 @@ var splide = new Splide(".main_slide", {
   // arrows: false,
   gap: "40px",
   start: 0,
-  autoplay: {
-    interval: 300,
-    pauseOnHover: {
-      resetProgress: true,
-    },
-  },
+  // autoplay: {
+  //   interval: 300,
+  //   pauseOnHover: {
+  //     resetProgress: true,
+  //   },
+  // },
 });
 
 splide.mount();
@@ -34,10 +34,10 @@ var swiper = new Swiper(".event_reser", {
   slidesPerView: 5,
   spaceBetween: 20,
   loop: true,
-  autoplay: {
-    delay: 3000,
-    disableOnInteraction: false,
-  },
+  // autoplay: {
+  //   delay: 3000,
+  //   disableOnInteraction: false,
+  // },
   scrollbar: {
     el: ".swiper-scrollbar",
     hide: false,
