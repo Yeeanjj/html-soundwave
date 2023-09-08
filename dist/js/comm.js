5a5756ade575a3827d0ee072faa932933a4c222a
Yeeanj <sesju0527@gamil.com>
2023-09-08 11:19:01 +0900

    pagenate 수정

--- dist/js/comm.js
+++ dist/js/comm.js
@@ -1,18 +1,18 @@
-// var splide = new Splide(".main_slide", {
-//   type: "loop",
-//   // perPage: 1,
-//   // arrows: false,
-//   gap: "40px",
-//   start: 0,
-//   autoplay: {
-//     interval: 250,
-//     pauseOnHover: {
-//       resetProgress: true,
-//     },
-//   },
-// });
+var splide = new Splide(".main_slide", {
+  type: "loop",
+  // perPage: 1,
+  // arrows: false,
+  gap: "40px",
+  start: 0,
+  autoplay: {
+    interval: 250,
+    pauseOnHover: {
+      resetProgress: true,
+    },
+  },
+});
 
-// splide.mount();
+splide.mount();
 
 var swiper = new Swiper(".event_reser", {
   slidesPerView: 5,
